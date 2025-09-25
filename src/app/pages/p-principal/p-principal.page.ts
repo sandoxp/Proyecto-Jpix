f1f2602059b391f9d84af8aef39b777bbcba4250
Sando <sandotmp@gmail.com>
2025-09-25 13:03:53 -0300

    standalones

--- src/app/pages/p-principal/p-principal.page.ts
+++ src/app/pages/p-principal/p-principal.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-p-principal',
   templateUrl: './p-principal.page.html',
   styleUrls: ['./p-principal.page.scss'],
+  standalone: false,
 })
 export class PPrincipalPage implements OnInit {
 
