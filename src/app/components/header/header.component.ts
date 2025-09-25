cbc67d19d8e418861e459697e43d34572006752c
sandoxp <sandotmp@gmail.com>
2025-09-25 20:46:32 -0300

    Ahora si funciona home a pPrincipal

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {}
+
+  goToHome(){
+    this.router.navigate(['/home']);
+  }
+
+  goToPPrincipal(){
+    this.router.navigate(['/p-principal']);
+  }
 
 }
