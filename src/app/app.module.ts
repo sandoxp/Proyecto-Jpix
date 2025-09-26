bcbbf54133db6cd52222d83f5fb8da1dc9beed95
sandoxp <sandotmp@gmail.com>
2025-09-25 21:22:27 -0300

    Se agregó el tap-bar semi funcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './components/header/header.component';
+import { TabsComponent } from './components/tabs/tabs.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, TabsComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
---
+++ src/app/components/tabs/tabs.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss'],
+  standalone: false,
+})
+export class TabsComponent  implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {}
+
+  goToHome(){
+    this.router.navigate(['/home']);
+  }
+
+  goToPPrincipal(){
+    this.router.navigate(['/p-principal']);
+  }
+
+}
